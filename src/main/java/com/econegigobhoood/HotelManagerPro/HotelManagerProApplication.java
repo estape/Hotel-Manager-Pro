aca9df76358a79a5bede65cffde47b83f28d19d8
Rodrigo Estape de Oliveira <rodrigo_estape@hotmail.com>
2024-05-09 15:02:22 -0300

    Add: First update!

---
+++ src/main/java/com/econegigobhoood/HotelManagerPro/HotelManagerProApplication.java
@@ -0,0 +1,13 @@
+package com.econegigobhoood.HotelManagerPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagerProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagerProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/econegigobhoood/HotelManagerPro/HotelManagerProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econegigobhoood.HotelManagerPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
