e37f81a3bb0ee8a7ffb902f5ceffac7177b078fc
Rodrigo Estape de Oliveira <rodrigo_estape@hotmail.com>
2024-05-13 16:09:52 -0300

    Fix: cleaning HotelManagerProApplication.java

--- src/main/java/com/econegigobhoood/HotelManagerPro/HotelManagerProApplication.java
+++ src/main/java/com/econegigobhoood/HotelManagerPro/HotelManagerProApplication.java
@@ -3,14 +3,10 @@ package com.econegigobhoood.HotelManagerPro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.econegigobhoood.HotelManagerPro.model.Misc;
-
 @SpringBootApplication
 public class HotelManagerProApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagerProApplication.class, args);
-
-		Misc.text(null, args);
 	}
 }
