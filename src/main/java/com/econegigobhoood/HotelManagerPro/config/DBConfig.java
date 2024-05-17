4e9d27ddd8e8fca4d02350ea24584a6f58c776e4
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-05-17 14:21:56 -0300

    minor_update: limpando DBConfig e acrescentando explicações - Limpeza de partes que não estão sendo utilizadas no DBConfig. - Acrescentada explicações em comentários para auxiliar na recordação e aprendizado das soluções tomadas

--- src/main/java/com/econegigobhoood/HotelManagerPro/config/DBConfig.java
+++ src/main/java/com/econegigobhoood/HotelManagerPro/config/DBConfig.java
@@ -8,10 +8,8 @@ import java.sql.Statement;
 // java.diversos.imports
   // Curiosidade: BufferedReader e FileReader são extendidas por Reader
 import java.io.BufferedReader; // Para realizar leitura em bloco e não caracter por caracter. Torna a leitura mais otimizada em caso de alta carga de leitura ou rede lenta
-import java.io.FileReader;
+import java.io.FileReader; // Lê um arquivo, buscando seus bytes e decodificando em letras através do decodificador do sistema/aplicativo
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class DBConfig {
@@ -25,19 +23,21 @@ public class DBConfig {
   }
 
   public static void createTables() {
-    // Use esse snipet mais para criar as tabelas, pois, ele considera ";" como separador para atribuir nas instrucoes.
-    // Em SQL real, um ponto e vírgula pode aparecer dentro de uma string literal e não deve ser tratado como o final de uma instrução. Para um tratamento correto de scripts SQL, podemos estudar, para outros projetos, bibliotecas como o jOOQ ou o JSR 356.
-    
     // estrutura Try-with-resources, ou seja, try(resources){ } catch{ }, automaticamente fecha o recurso declarado no final do bloco.
     try (
-      // InputStream is = DBConfig.class.getResourceAsStream("/model_f_sgh.sql");
+      // >>>>>> Lendo arquivos SQL na pasta config <<<<<<
       BufferedReader reader = new BufferedReader(new FileReader("src\\main\\java\\com\\econegigobhoood\\HotelManagerPro\\config\\model_f_sgh.sql"));
       Connection conexao = getConnection();
       Statement stmt = conexao.createStatement()
-    ) {
+      ) {
+      // >>>>>> Preparando elementos buscados no SQL para execução <<<<<<
       String conteudo = reader.lines().collect(Collectors.joining("\n"));
+        // [!] .lines() retorena um Stream<string> que corresponde a uma linha de texto do BufferedReader, não incluindo EOF
+        // [!] .collect() é uma operação terminal da API Stream, usado para transformar elementos de um stream em uma estrutura de dados diferente ou valor agregado, recebendo um Collectors. Ou seja, Collectors.toList(ponto_juncao) transforma Stream em Lista, Collectors.joining() junta um Stream<String> em uma única String no ponto de junção colocado em seu parâmetro. Por exemplo, "\n" como ponto de junção juntará todas as linhas que receberem um caracter de pula-linha transformando-o tudo em uma string só.
       String[] instrucoes = conteudo.split(";");
-
+        // [!] Use esse snipet mais para criar as tabelas, pois, ele está considerando ";" como separador para atribuir nas instrucoes.
+        // [!] .split separará a String em substrings partindo-as a cada ";". Como no nosso caso o ";" indica o final de cada tabela, String[] instrução passa a virar uma array de tabelas a criar
+        // [!] Em SQL real, um ponto e vírgula pode aparecer dentro de uma string literal e não deve ser tratado como o final de uma instrução. Para um tratamento correto de scripts SQL, podemos estudar, para outros projetos, bibliotecas como o jOOQ ou o JSR 356.
       for (String instrucao : instrucoes) {
           stmt.execute(instrucao);
       }
